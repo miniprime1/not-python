2989e7ada23037c823a1ec963f5f31ae44cff466
miniprime1 <64111105+miniprime1@users.noreply.github.com>
2021-04-24 11:09:09 +0900

    Add files via upload

---
+++ darwin/not-python-3.7.8-darwin.py
@@ -0,0 +1,7 @@
+print('Not Python 3.7.8 (v3.7.8:4b47a5b6ba, Jun 27 2020, 04:47:50)')
+print('[Clang 6.0 (clang-600.0.57)] on darwin')
+print('Type "help", "copyright", "credits" or "license" for more information.')
+while True:
+    i = input(">>> ")
+    if i == "exit()": break
+    print("Nope!")
---
+++ linux/not-python-3.7.8-linux.py
@@ -0,0 +1,7 @@
+print('Not Python 3.7.8 (v3.7.8:4b47a5b6ba, Jun 27 2020, 09:53:43)')
+print('[GCC 8.4.0] on linux')
+print('Type "help", "copyright", "credits" or "license" for more information.')
+while True:
+    i = input(">>> ")
+    if i == "exit()": break
+    print("Nope!")
