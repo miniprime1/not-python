73f32ae5442a1520277be4dcfe1f845df7767cc0
miniprime1 <64111105+miniprime1@users.noreply.github.com>
2021-04-24 11:07:35 +0900

    Add files via upload

---
+++ win/win_multi-exe/not-python-3.7.8-win32.py
@@ -0,0 +1,6 @@
+print('Not Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 07:55:33) [MSC v.1916 32 bit (Intel)] on win32')
+print('Type "help", "copyright", "credits" or "license" for more information.')
+while True:
+    i = input(">>> ")
+    if i == "exit()": break
+    print("Nope!")
