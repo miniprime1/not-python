6149bc4ad6a813945d88cbc75931d7967262464b
miniprime1 <64111105+miniprime1@users.noreply.github.com>
2021-05-15 22:54:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if platform.system() == "Windows":
         if i == "exit()": break
         print("Nope!")
 
-elif platform.system() == "Drawin":
+elif platform.system() == "Drarwin":
     print('Not Python 3.7.8 (v3.7.8:4b47a5b6ba, Jun 27 2020, 04:47:50)')
     print('[Clang 6.0 (clang-600.0.57)] on darwin')
     print('Type "help", "copyright", "credits" or "license" for more information.')
